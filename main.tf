6a31bd71c7840ca4d15e80ae97380cf93c83841b
unknown <pranav.jenus@gmail.com>
2025-11-21 10:47:12 +0530

    Aws s3 bucket on tf cloud

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">=1.0"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = ">=5.30"
+    }
+  }
+}
+
+resource "random_id" "rand_id" {
+  byte_length = 8
+}
+
+resource "aws_s3_bucket" "tf-bucket-cloud" {
+  bucket = "tf-cloud-s3-bucket-${random_id.rand_id.hex}"
+}
